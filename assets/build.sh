81d56d09c0bc18fb4ec00c6ab437a40db7a70c68
hipinzhao <zhaohaibin@outlook.com>
2015-09-07 23:15:43 +0800

    build shell

--- assets/build.sh
+++ assets/build.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 echo "build at `date`"
 cd /root/blog
-git remote add origin {{GITHUB_REMOTE_ADDR}}
-git reset --hard origin/master
 git clean -f
 git pull
-git checkout master
+git fetch
+git checkout b master origin/master
 hexo clean
 hexo d -g
 echo "built successfully"
